3a0053a1200677274ef81ed3f5123d3f3d8ff0e9
Maicon R. Silva <maiconrods@gmail.com>
2020-06-19 15:04:25 -0300

    (errors) Handling Exceptions: treating errors and exceptions.

--- src/application.ts
+++ src/application.ts
@@ -5,8 +5,11 @@ import * as bodyParser from 'body-parser';
 import * as cors from 'cors';
 import * as dotenv from 'dotenv';
 import './container';
+import { errors } from 'celebrate';
+import 'express-async-errors';
 
 import routes from './routes';
+import AppError from './errors/AppError';
 
 dotenv.config();
 
@@ -16,8 +19,23 @@ application.use(bodyParser.text());
 application.use(express.json());
 application.use(express.urlencoded({ extended: false }));
 application.use(cors());
-
 application.use(routes);
+
+application.use(errors());
+
+application.use((err: Error, request: express.Request, response: express.Response, _: express.NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      error: 'error',
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    error: 'error',
+    message: err.message,
+  });
+});
 
 application.set('port', process.env.APP_PORT || 5000);
 
